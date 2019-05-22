a0c0bdc60aca75e91cd49d7bd32252c392a0cf7d
Admin <Admin@DESKTOP-89NTJEN>
2019-05-22 13:58:29 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempconverter
+{
+    //Program to convert from Celsius degrees to Kelvin and Fahrenheit
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.Write("Enter the celsius: ");
+                int celsius = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", celsius + 273); // 1°C + 273.15 = 274.15K
+
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32); //(1°C × 9/5) + 32 = 33.8°F
+
+
+
+        }
+    }
+}
